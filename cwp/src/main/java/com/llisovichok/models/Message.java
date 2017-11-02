4532e23c6978b43a2d847399b8dcb99b43c153ed
128500 <llisovichok@gmail.com>
2017-11-02 20:26:17 +0200

    Temporary point

--- cwp/src/main/java/com/llisovichok/models/Message.java
+++ cwp/src/main/java/com/llisovichok/models/Message.java
@@ -1,5 +1,7 @@
 package com.llisovichok.models;
 
+import java.util.Objects;
+
 /**
  * Created by KUDIN ALEKSANDR on 21.07.2017.
  */
@@ -37,5 +39,20 @@ public class Message {
 
     public void setText(String text){
         this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return Objects.equals(id, message.id) &&
+                Objects.equals(user, message.user) &&
+                Objects.equals(text, message.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user, text);
     }
 }
